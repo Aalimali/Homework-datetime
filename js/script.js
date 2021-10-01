e4947d8e5769e4cbd99391b07f07e794cc8a657e
Aalimali <90587874+Aalimali@users.noreply.github.com>
2021-10-01 00:15:36 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 $("#currentDay").text(moment().format('LLLL'));
 
+$(".saveBtn").click(function(){
+    var textAreaVal = $(this).siblings("textarea").val();
+    var key = $(this).parent().attr("id");
+    localStorage.setItem(key, textAreaVal);
+});
+
+// $("#time9 textarea").val(localStorage.getItem("time9"));
+// $("#time10 textarea").val(localStorage.getItem("time10"));
+// $("#time11 textarea").val(localStorage.getItem("time11"));
+// $("#time12 textarea").val(localStorage.getItem("time12"));
+// $("#time13 textarea").val(localStorage.getItem("time13"));
+// $("#time14 textarea").val(localStorage.getItem("time14"));
+// $("#time15 textarea").val(localStorage.getItem("time15"));
+// $("#time16 textarea").val(localStorage.getItem("time16"));
+// $("#time17 textarea").val(localStorage.getItem("time17"));
+
+for (var i = 1; i <= 12; i++){
+    if(i != 6 && i != 7 && i !=8){
+        $("#time" + i + " textarea").val(localStorage.getItem("time" + i));
+    }
+
+
+}
+
 
 function updateTextArea(){
     var hourNow = moment().hours(); 
